11f0d6f3f684dca45aead3ea512a2035c7fa3393
crack-cow <antoni.machowski@gmail.com>
2015-09-27 23:48:26 +0200

    written functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a list of functions similar to the one
+## used in the example (makeVector function):
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix
+
+
+
+## We define m as the "empty" inverse of x and we define following
+## functions:
+## set(y) - sets y as the value of the matrix and
+##          "cleans the cache"
+## get() - gets the value of the matrix
+## setinv(inv) - sets inv as the value of the inversion
+## getinv() - gets the value of the inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## The argument x is a list prepaired by makeCacheMatrix.
+## First we get cached inversion and assign it to variable m.
+## If the cache was not empty, we just return it.
+## If it was, we calculate the inversion, set it and return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        mat <- x$get()
+        m <- solve(mat, ...)
+        x$setinv(m)
+        m
 }
